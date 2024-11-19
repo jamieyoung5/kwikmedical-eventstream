eb25b8225053163d1fca42b66ac51b2798cbc736
jamieyoung5 <Jamie.Young@n-able.com>
2024-11-19 01:43:30 +0000

    patch: fix eventstream_grpc import

--- pb/eventstream_grpc.pb.go
+++ pb/eventstream_grpc.pb.go
@@ -8,7 +8,7 @@ package pb
 
 import (
 	context "context"
-	v1 "github.com/kwikmedical-eventstream/pb/io/cloudevents/v1"
+	v1 "github.com/jamieyoung5/kwikmedical-eventstream/pb/io/cloudevents/v1"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
