39bdebe43e5bd0eef5eddd739d78d6e35a69fed9
jamieyoung5 <jamie5young55@gmail.com>
2024-11-21 03:11:52 +0000

    patch: add wait in start server retry loop

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	eventstreamserver "github.com/jamieyoung5/kwikmedical-eventstream/cmd/eventstream"
 	"os"
+	"time"
 )
 
 func main() {
@@ -13,6 +14,8 @@ func main() {
 		if err != nil {
 			os.Exit(1)
 		}
+
+		time.Sleep(5 * time.Second)
 	}
 
 }
